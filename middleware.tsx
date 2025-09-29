5c254901efc58ff31c1802f33d275bed669854c4
tsoji78 <tsojivictor@gmail.com>
2025-09-28 18:08:27 -1100

    middle waressssss

--- middleware.tsx
+++ middleware.tsx
@@ -11,6 +11,9 @@ export function middleware(request: NextRequest) {
     '/',
     '/about',
     '/catalogue',
+    '/modules',           // Add this
+    '/modules/addcourse', // Add this
+    '/courses',
     
     // Add other public routes if necessary
   ];
@@ -34,5 +37,5 @@ export function middleware(request: NextRequest) {
 
 // Apply middleware to all routes except the specified public paths
 export const config = {
-  matcher: ['/:path*'], // Adjust based on your needs
+  matcher: ['//:path*'], // Adjust based on your needs
 };
